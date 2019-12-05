aaa24cf326e7e61b7b4b023e5d20815ef3c56818
Clément <clement.evd.dev@gmail.com>
2019-12-05 17:17:18 +0100

    ajout de breakpoint au cas ou, menu en Js et JQ, commentaires

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 /* navigation */
 
-$(function() {
-  
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("toggle-above").addEventListener('click', function(e) { 
+    let burger = document.getElementById("toggle");
+    burger.classList.toggle("on");
+    document.getElementById("resize").classList.toggle("active"); 
+  })
+});
+
+/* ------------ */
+/* et en Jquery */
+/* ------------ */
+
+/* $(function() {
     $("#toggle").click(function() {
       $(this).toggleClass("on");
       $("#resize").toggleClass("active");
-    });
-    
-  });
-  
+    });   
+  }); */
   
 /* Sticky */
-var stickybit = stickybits('.caption-right');
+stickybits('.caption-right-img',{
+  stickyBitStickyOffset: 200,
+});
+stickybits('.bgimg-2-caption',{
+  stickyBitStickyOffset: 200,
+});
